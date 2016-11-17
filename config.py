ac2e96046f5f80b90ba60e04f20b374f8073883c
LittleDan45 <littledan45@gmail.com>
2016-11-17 12:19:20 -0500

    changed configs and Fixes #1

--- config.py
+++ config.py
@@ -14,5 +14,4 @@ class Config(object):
     LOGGING_SERVERS = [Server(name='Fighting Server', domain='play.server.com', port=25565),
                        Server(name='Testing Server', domain='test.server.com', port=25565)]
     '''
-    LOGGING_SERVERS = [Server(name='Bonecrack', domain='connect.bonecrack.com', port=25565),
-                       Server(name='Nationcraft', domain='play.nationmc.net', port=25565)]
+    LOGGING_SERVERS = [Server(name='Nationcraft', domain='play.nationmc.net', port=25565)]
